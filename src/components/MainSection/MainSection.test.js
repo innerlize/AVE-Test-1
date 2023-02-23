bc27d030f3a651a42f771e7326df0f585483ea4b
Alan <tiltedveggie@outlook.kr>
2023-02-23 03:31:38 -0300

    Test Update

--- src/components/MainSection/MainSection.test.js
+++ src/components/MainSection/MainSection.test.js
@@ -5,14 +5,13 @@ import MainSection from './MainSection.js';
 import SearchBar from './SearchBar/SearchBar.js';
 import FiltersBox from './FiltersBox/FiltersBox.js';
 import axios from 'axios';
+import mockProducts from '../../Database/mockProducts.js';
 
 jest.mock('axios');
 
-beforeAll(() => {
-	render(<MainSection />);
-});
-
 test('Check if renders the Search Bar', () => {
+	render(<SearchBar />);
+
 	const textInput = screen.getByTestId('text-input');
 
 	expect(textInput).toBeInTheDocument();
@@ -35,27 +34,23 @@ test('Check if renders the Filters Box', () => {
 });
 
 test('Check if renders all the 20 items in the MainSection', async () => {
-	axios.get.mockReturnValueOnce({
-		data:
-	})
+	axios.get.mockResolvedValue({
+		data: mockProducts,
+	});
 
 	render(<MainSection />);
 
-	const itemsBoxes = await screen.findAllByRole('listitem', undefined, {
-		timeout: 3000,
-	});
+	const allBoxes = await screen.findAllByRole('listitem');
 
-	expect(itemsBoxes).toHaveLength(20);
+	expect(allBoxes).toHaveLength(20);
 });
 
 // -----------------------------------------------------------------------------
 
 test('Check SearchBar functionality', async () => {
-	const setSearch = jest.fn();
-
 	render(
 		<MainSection>
-			<SearchBar setSearch={setSearch} />
+			<SearchBar />
 		</MainSection>
 	);
 
@@ -63,19 +58,15 @@ test('Check SearchBar functionality', async () => {
 
 	fireEvent.change(textInput, { target: { value: 'mens cotton jacket' } });
 
-	const itemBoxExpected = await screen.findByText(
-		/mens cotton jacket/i,
-		undefined,
-		{ timeout: 3000 }
-	);
+	const itemBoxExpected = await screen.findByText(/mens cotton jacket/i);
 
-	const itemsBoxesArray = await screen.findAllByRole('listitem', undefined, {
-		timeout: 3000,
-	});
+	const itemsBoxesArray = await screen.findAllByRole('listitem');
 
 	expect(itemBoxExpected).toBeInTheDocument();
 	expect(itemsBoxesArray).toHaveLength(1);
 });
+
+// -----------------------------------------------------------------------------
 
 test('Check Filters functionality I (sort by Price)', async () => {
 	render(
@@ -89,17 +80,14 @@ test('Check Filters functionality I (sort by Price)', async () => {
 		screen.getByRole('option', { name: /ascending/i })
 	);
 
-	await waitFor(
-		() => {
-			const itemsBoxesArray = screen.getAllByRole('listitem');
+	await waitFor(() => {
+		const itemsBoxesArray = screen.getAllByRole('listitem');
 
-			expect(itemsBoxesArray).toHaveLength(20);
-			expect(itemsBoxesArray[0]).toHaveTextContent('$7.95');
-			expect(itemsBoxesArray[1]).toHaveTextContent('$9.85');
-			expect(itemsBoxesArray[2]).toHaveTextContent('$9.99');
-		},
-		{ timeout: 3000 }
-	);
+		expect(itemsBoxesArray).toHaveLength(20);
+		expect(itemsBoxesArray[0]).toHaveTextContent('$7.95');
+		expect(itemsBoxesArray[1]).toHaveTextContent('$9.85');
+		expect(itemsBoxesArray[2]).toHaveTextContent('$9.99');
+	});
 });
 
 test('Check Filters functionality II (sort by Category)', async () => {
@@ -114,24 +102,19 @@ test('Check Filters functionality II (sort by Category)', async () => {
 		screen.getByRole('option', { name: /jewelery/i })
 	);
 
-	await waitFor(
-		() => {
-			const itemsBoxesArray = screen.getAllByRole('listitem');
+	await waitFor(() => {
+		const itemsBoxesArray = screen.getAllByRole('listitem');
 
-			expect(itemsBoxesArray).toHaveLength(4);
-			expect(itemsBoxesArray[0]).toHaveTextContent(
-				/john hardy women's legends naga gold & silver dragon station chain bracelet/i
-			);
-			expect(itemsBoxesArray[1]).toHaveTextContent(
-				/solid gold petite micropave/i
-			);
-			expect(itemsBoxesArray[2]).toHaveTextContent(
-				/white gold plated princess/i
-			);
-			expect(itemsBoxesArray[3]).toHaveTextContent(
-				/pierced owl rose gold plated stainless steel double/i
-			);
-		},
-		{ timeout: 3000 }
-	);
+		expect(itemsBoxesArray).toHaveLength(4);
+		expect(itemsBoxesArray[0]).toHaveTextContent(
+			/john hardy women's legends naga gold & silver dragon station chain bracelet/i
+		);
+		expect(itemsBoxesArray[1]).toHaveTextContent(
+			/solid gold petite micropave/i
+		);
+		expect(itemsBoxesArray[2]).toHaveTextContent(/white gold plated princess/i);
+		expect(itemsBoxesArray[3]).toHaveTextContent(
+			/pierced owl rose gold plated stainless steel double/i
+		);
+	});
 });
